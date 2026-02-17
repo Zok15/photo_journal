11419f12a9983b821068336349f784ea01593464
Катерина Сальникова <myasoezhka@gmail.com>
2026-02-17 18:20:12 +0400

    Add rate limits for auth API endpoints

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,8 @@ use App\Http\Controllers\Api\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::post('auth/register', [AuthController::class, 'register']);
-    Route::post('auth/login', [AuthController::class, 'login']);
+    Route::post('auth/register', [AuthController::class, 'register'])->middleware('throttle:5,1');
+    Route::post('auth/login', [AuthController::class, 'login'])->middleware('throttle:10,1');
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('auth/logout', [AuthController::class, 'logout']);
