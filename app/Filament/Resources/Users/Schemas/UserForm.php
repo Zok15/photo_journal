acd26c2e06fb549dba7cb3056a6bf5034aa6e3ba
Катерина Сальникова <myasoezhka@gmail.com>
2026-02-23 15:15:13 +0400

    Allow editing email verification in admin users

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Users\Schemas;
 
+use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
@@ -20,6 +21,11 @@ class UserForm
                     ->required()
                     ->maxLength(255)
                     ->unique(ignoreRecord: true),
+                DateTimePicker::make('email_verified_at')
+                    ->label('Email verified at')
+                    ->seconds(false)
+                    ->native(false)
+                    ->nullable(),
                 TextInput::make('password')
                     ->password()
                     ->required(fn (string $operation): bool => $operation === 'create')
--- app/Filament/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Users\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -21,6 +22,10 @@ class UsersTable
                     ->sortable(),
                 TextColumn::make('email')
                     ->searchable()
+                    ->sortable(),
+                IconColumn::make('email_verified_at')
+                    ->label('Email verified')
+                    ->boolean()
                     ->sortable(),
                 TextColumn::make('roles.name')
                     ->badge()
